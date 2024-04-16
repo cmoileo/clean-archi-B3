03370767bbaad95985c97e29533019d981c5f8e5
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-16 10:49:52 +0200

    fix test

--- src/order/domain/use-case/set-order-shipping-method.spec.ts
+++ src/order/domain/use-case/set-order-shipping-method.spec.ts
@@ -1,8 +1,10 @@
 import { Order } from '../entity/order.entity';
 import { OrderRepositoryInterface } from '../port/order.repository.interface';
 import { PaidOrderService } from './paid-order.service';
+import { OrderStatus } from '../enum/order-status.enum';
+import { SetOrderShippingAddressService } from './set-order-shipping-method.service';
 describe('paid order', () => {
-  const order = new Order('John Doe', []);
+  const order = new Order('ordername', []);
   const orderRepositoryMock = {
     findById() {
       return order;
@@ -11,15 +13,30 @@ describe('paid order', () => {
       return order;
     },
   } as any as OrderRepositoryInterface;
-  order.setShippingAddress('123 Main St');
+  order.setShippingAddress('address');
   it('should update an order to paid', async () => {
-    const paidOrderService = new PaidOrderService(orderRepositoryMock);
-    const updatedOrder = await paidOrderService.paidOrder('123');
-    expect(updatedOrder.status).toBe('PAID');
-  });
-  it('should throw an error if shipping address is empty', () => {
-    expect(() => order.setShippingAddress('')).toThrowError(
-      'Shipping address is required',
+    const setOrderShippingAddressService = new SetOrderShippingAddressService(
+      orderRepositoryMock,
     );
+
+    const updatedOrder =
+      await setOrderShippingAddressService.setOrderShippingAddress({
+        orderId: 'order',
+        shippingAddress: 'address',
+      });
+    expect(updatedOrder.shippingAddress).toBe('123 Main St.');
+    expect(updatedOrder.status).toBe(OrderStatus.SHIPPING_ADDRESS_SET);
+  });
+  it('should throw an error if shipping address is empty', async () => {
+    try {
+      await new SetOrderShippingAddressService(
+        orderRepositoryMock,
+      ).setOrderShippingAddress({
+        orderId: 'order',
+        shippingAddress: '',
+      });
+    } catch (error) {
+      expect(error.message).toBe('Shipping address is required');
+    }
   });
 });
